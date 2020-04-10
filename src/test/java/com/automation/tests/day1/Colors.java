14cd18c39176fe23f7edf288dfa79ade5c46f94b
55lala <lalaimanovaus@gmail.com>
2020-04-10 15:52:10 -0400

    love

--- src/test/java/com/automation/tests/day1/Colors.java
+++ src/test/java/com/automation/tests/day1/Colors.java
@@ -2,5 +2,5 @@ package com.automation.tests.day1;
 
 public class Colors {
 
-    String[] colors = {"red", "blue", "green","yellow"};
+    String[] colors = {"red", "blue", "green","yellow", "love"};
 }
